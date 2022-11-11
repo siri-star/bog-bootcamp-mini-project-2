0460e79e61839ed41732aacc3a9980d4e230cd98
Siri Tanguturi <siri.tanguturi1@gmail.com>
2022-11-11 00:46:21 -0500

    making history pageable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import History from './components/History'
-import React, {useState} from 'react';
+import History from './components/History';
+import React, {useState, useEffect} from 'react';
 
 function App() {
   const[catFacts, setCatFacts] = useState([{fact: "A kitten will typically weigh about 3 ounces at birth.  The typical male housecat will weigh between  7 and 9 pounds, slightly less for female housecats.", isFavorite: false}]);
+  const [pageNum, setPageNum] = useState(0);
+  
   async function getCatFact() {
     const response = await fetch('https://catfact.ninja/fact');
     const data = await response.json();
-    setCatFacts([{fact: data.fact, isFavorite: false}, ...catFacts])
+    const newCatFact = {fact: data.fact, isFavorite: false};
+    setCatFacts([{fact: data.fact, isFavorite: false}, ...catFacts]);
   }
 
   function newFactHandler(e) {
@@ -16,9 +19,28 @@ function App() {
 
   return (
     <div className="App">
+
       <h1>{catFacts[0].fact}</h1>
+
       <button onClick={newFactHandler}>new cat fact :D</button>
-      <History catFacts={catFacts} />
+
+      <History catFacts = {catFacts} pageNum = {pageNum}/>
+
+      {/* TODO: (readability) move pagination stuff to its own component */}
+      <button onClick = {e => {
+        if (pageNum > 0) {
+          setPageNum(pageNum - 1);
+        }
+      }}>previous</button>
+
+      <button onClick={e => {
+        if (10 * (pageNum + 1) < catFacts.length) {
+          setPageNum(pageNum + 1);
+        }
+      }}>next</button>
+
+      <p>page {pageNum + 1} out of {Math.floor(catFacts.length / 10 + 1)}</p>
+
     </div>
   );
 }
--- src/components/History.js
+++ src/components/History.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function History({catFacts}) {
-    return catFacts.map(catFact => <p>{catFact.fact}</p>)
+export default function History(props) {
+    const ret = [];
+    for (let i = props.pageNum * 10 + 1; i < (props.pageNum + 1) * 10 && i < props.catFacts.length; i++) {
+        ret[i] = <p>{props.catFacts[i].fact}</p>;
+    }
+    return ret;
 }
