d399cfc0cf4aaacced5c5c2223a87c89117c9185
Siri Tanguturi <siri.tanguturi1@gmail.com>
2022-11-11 13:42:48 -0500

    current fact component

---
+++ src/components/CurrentFact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function CurrentFact(props) {
+
+    async function getCatFact() {
+        console.log("here");
+        const response = await fetch('https://catfact.ninja/fact');
+        const data = await response.json();
+        props.setCatFacts([{fact: data.fact, isFavorite: false}, ...props.catFacts]);
+    }
+
+    const fact = <h1>{props.catFacts[0].fact}</h1>
+    const button = <button onClick={e => getCatFact()}>new cat fact :D</button>
+
+    return [fact, button];
+}
