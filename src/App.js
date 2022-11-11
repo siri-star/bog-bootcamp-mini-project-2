eee4dd41279be9631fdfe5fa6a7283d470206ccc
Siri Tanguturi <siri.tanguturi1@gmail.com>
2022-11-10 19:16:59 -0500

    display new cat fact

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App() {
-  const[catFacts, setCatFacts] = useState([]);
+  const[catFacts, setCatFacts] = useState([{fact: "A kitten will typically weigh about 3 ounces at birth.  The typical male housecat will weigh between  7 and 9 pounds, slightly less for female housecats.", isFavorite: false}]);
   async function getCatFact() {
     const response = await fetch('https://catfact.ninja/fact');
     const data = await response.json();
@@ -15,9 +15,8 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
-        <button onClick={newFactHandler}>new cat fact :D</button>
-      </div>
+      <h1>{catFacts[0].fact}</h1>
+      <button onClick={newFactHandler}>new cat fact :D</button>
     </div>
   );
 }
