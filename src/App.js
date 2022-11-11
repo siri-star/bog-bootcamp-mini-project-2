a62a6d3a985dd6ccb9347da8805132fa11d8e6f4
Siri Tanguturi <siri.tanguturi1@gmail.com>
2022-11-10 19:14:23 -0500

    getting new cat fact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const[catFacts, setCatFacts] = useState([]);
+  async function getCatFact() {
+    const response = await fetch('https://catfact.ninja/fact');
+    const data = await response.json();
+    setCatFacts([{fact: data.fact, isFavorite: false}, ...catFacts])
+  }
+
+  function newFactHandler(e) {
+    getCatFact();
+  }
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="container">
+        <button onClick={newFactHandler}>new cat fact :D</button>
+      </div>
+    </div>
   );
 }
 
